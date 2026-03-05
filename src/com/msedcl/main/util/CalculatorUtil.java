876581ffdd552b5d8f7e873fec28d1f4ce2ce653
student5 <student5@host.docker.internal>
2026-03-05 12:15:52 +0530

    Change addition method message

--- src/com/msedcl/main/util/CalculatorUtil.java
+++ src/com/msedcl/main/util/CalculatorUtil.java
@@ -2,6 +2,7 @@ package com.msedcl.main.util;
 
 public class CalculatorUtil {
 	public int addition(int number1, int number2) {
+		System.out.println("Adding two number");
 		return number1 + number2;
 	}
 
