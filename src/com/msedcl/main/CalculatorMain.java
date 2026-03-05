e6f97728b9994a2414d2a7638aef0422f48d55e3
student12 <student12@host.docker.internal>
2026-03-05 11:59:56 +0530

    new multiplication feauture implemeneted

--- src/com/msedcl/main/CalculatorMain.java
+++ src/com/msedcl/main/CalculatorMain.java
@@ -8,6 +8,7 @@ public class CalculatorMain {
 		CalculatorUtil calculatorUtil = new CalculatorUtil();
 		System.out.println("Addition = " + calculatorUtil.addition(10, 20));
 		System.out.println("Subtraction = " + calculatorUtil.subtraction(10, 2));
+		System.out.println("Multiplication = " + calculatorUtil.multiplication(10, 6));
 	}
 
 }
