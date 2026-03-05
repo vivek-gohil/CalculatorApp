f32d244fa4412333aaf419e5dc3c210475d215c7
student12 <student12@host.docker.internal>
2026-03-05 12:15:51 +0530

    print line added to check conflict

--- src/com/msedcl/main/util/CalculatorUtil.java
+++ src/com/msedcl/main/util/CalculatorUtil.java
@@ -2,7 +2,9 @@ package com.msedcl.main.util;
 
 public class CalculatorUtil {
 	public int addition(int number1, int number2) {
-		return number1 + number2;
+		System.out.println("Result is ");
+		return number1 + number2+0;
+		
 	}
 
 	public int subtraction(int number1, int number2) {
